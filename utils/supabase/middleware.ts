bd03b0c36d33fc43d47727ec951672fc6df7a800
anthonylbm69 <antonex.ly@gmailcom>
2025-03-02 19:11:36 +0100

    ..

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -7,8 +7,8 @@ export async function updateSession(request: NextRequest) {
     })
 
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        "https://kmdpmjamxoigctqdogtz.supabase.com",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttZHBtamFteG9pZ2N0cWRvZ3R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNzYzODIsImV4cCI6MjA1NDg1MjM4Mn0.rl_lYU3bP-MjuqXFfk-YwFVDNxfJnrwUEp2Tfs727dY",
         {
             cookies: {
                 getAll() {
